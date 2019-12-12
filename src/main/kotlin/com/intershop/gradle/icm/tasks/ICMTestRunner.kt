d5051540bb4b41420e6602f4aa5a01f9a53a346f
Matthias Raab <m.raab@intershop.de>
2019-12-12 13:55:10 +0100

    fix issue with runtime, add tasks and extension

--- src/main/kotlin/com/intershop/gradle/icm/tasks/ICMTestRunner.kt
+++ src/main/kotlin/com/intershop/gradle/icm/tasks/ICMTestRunner.kt
@@ -106,6 +106,7 @@ class ICMTestRunner : JavaExec() {
      *
      * @property testhome directory path
      */
+    @get:Input
     var testhome: File?
         get()  {
             if(testhomeProperty.orNull != null) {
