fac345000bf277d617298b928213618e35d0977a
Matthias Raab <m.raab@intershop.de>
2020-06-06 11:25:22 +0200

    add method for properties in development properties

--- src/main/kotlin/com/intershop/gradle/icm/extension/DevelopmentConfiguration.kt
+++ src/main/kotlin/com/intershop/gradle/icm/extension/DevelopmentConfiguration.kt
@@ -24,6 +24,7 @@ import org.gradle.wrapper.GradleUserHomeLookup
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.File
+import java.util.*
 import javax.inject.Inject
 
 /**
@@ -49,10 +50,14 @@ open class DevelopmentConfiguration
 
         const val DEFAULT_LIC_PATH = "icm-default/lic"
         const val DEFAULT_CONFIG_PATH = "icm-default/conf"
+
+        const val LICENSE_FILE_NAME = "license.xml"
+        const val CONFIG_FILE_NAME = "cluster.properties"
     }
 
     private val licenseDirectoryProperty: Property<String> = objectFactory.property(String::class.java)
     private val configDirectoryProperty: Property<String> = objectFactory.property(String::class.java)
+    private val configProperties: Properties = Properties()
 
     init {
         // read environment
@@ -96,7 +101,10 @@ open class DevelopmentConfiguration
 
         licenseDirectoryProperty.set(licDirPath)
         configDirectoryProperty.set(configDirPath)
+        configProperties.load(File(configDirectory, CONFIG_FILE_NAME).inputStream())
     }
+
+
 
     /**
      * License directory path of the project.
@@ -105,7 +113,7 @@ open class DevelopmentConfiguration
         get() = licenseDirectoryProperty.get()
 
     val licenseFilePath: String
-        get() = File(licenseDirectory, "license.xml").absolutePath
+        get() = File(licenseDirectory, LICENSE_FILE_NAME).absolutePath
 
     /**
      * Local configuration path of the project.
@@ -114,5 +122,9 @@ open class DevelopmentConfiguration
         get() = configDirectoryProperty.get()
 
     val configFilePath: String
-        get() = File(configDirectory, "cluster.properties").absolutePath
+        get() = File(configDirectory, CONFIG_FILE_NAME).absolutePath
+
+    fun getConfigProperty(property: String): String {
+        return configProperties.getProperty(property, "")
+    }
 }
